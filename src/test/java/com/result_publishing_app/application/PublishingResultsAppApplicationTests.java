f2adb2aa60ebd97d49c97a18326197c91fc07d97
SmartW0rker <hristijancocevski@yahoo.com>
2023-02-08 06:02:00 +0100

    Initial commit

---
+++ src/test/java/com/result_publishing_app/application/PublishingResultsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.result_publishing_app.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishingResultsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
